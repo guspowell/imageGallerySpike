168505135f6ad0b33e8826d0a8b2de4af25e2882
guspowell <augustuspowell@me.com>
2015-06-01 14:51:44 +0100

    masonry working. not 100%

--- js/mansonry.js
+++ js/mansonry.js
@@ -1,6 +1,35 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // options...
-  itemSelector: '.item',
-  columnWidth: 200
+// $(document).ready(function() {
+//
+
+//
+//   $('.grid').isotope({
+//     // set itemSelector so .grid-sizer is not used in layout
+//     itemSelector: '.grid-item',
+//     percentPosition: true,
+//     masonry: {
+//       // use element for option
+//       columnWidth: '.grid-sizer'
+//     }
+//   })
+//
+//   $('.item').masonry()
+//
+// });
+
+$(document).ready( function() {
+
+  var container = document.querySelector('.grid');
+  var msnry = new Masonry( container, {
+    // options...
+    itemSelector: '.item',
+    columnWidth: 100
+  });
+
+  $('.grid').isotope({
+    itemSelector: '.grid-item',
+    masonry: {
+      columnWidth: 100
+    }
+  });
+
 });
