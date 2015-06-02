cec5442be6482ff88581c58f79316e9f896eb405
guspowell <augustuspowell@me.com>
2015-06-02 09:38:49 +0100

    flutter effect working

--- js/global2.js
+++ js/global2.js
@@ -8,15 +8,16 @@ $(document).ready( function() {
   // fade out effect
 
   $(".grid-item").click(function() {
-    $(".grid div").not(this).fadeOut('fast');
-    $(".grid").css({background:"transparent"});
+
+    $(function() {
+      $('.grid div').sort(function(){
+        return Math.random()*10 > 5 ? 1 : -1;
+      }).each(function(i) {
+        $(this).delay((i++) * 100).fadeOut(200);
+      });
+    });
+
   });
 
-});
 
-// function showCarousel() {
-//   var position = $(".grid").position();
-//   var top = position['top'];
-//   var left = position['left'];
-//   $("#carousel.container").css({display:'inline', top: top, left: left, position:'fixed'});
-// };
+});
