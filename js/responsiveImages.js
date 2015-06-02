0717dc0970b584929a6d57d8d2ae98097bfa384e
guspowell <augustuspowell@me.com>
2015-06-02 14:58:36 +0100

    fixed column issue

--- js/responsiveImages.js
+++ js/responsiveImages.js
@@ -13,11 +13,12 @@ $(document).ready(function() {
 
 	function getNumColumns(){
 		var winWidth = $(window).width();
+		console.log(winWidth);
 		var column = 3;
 		if (winWidth<380) column = 1;
-		else if(winWidth >=380 && winWidth<1160)  column = 2;
-		else if(winWidth>=1160 && winWidth<1640)  column = 3;
-		else if(winWidth>=1640  && winWidth<2100) column = 4;
+		else if(winWidth >=380 && winWidth<850)  column = 2;
+		else if(winWidth>=850 && winWidth<1400)  column = 3;
+		else if(winWidth>=1400  && winWidth<2000) column = 4;
 		else if(winWidth>=2100) column = 5;
 		return column;
 	}
@@ -26,23 +27,11 @@ $(document).ready(function() {
 		var columns = getNumColumns();
 		var containerWidth = $(window).width();
 		var postWidth = containerWidth/columns;
-    // var largePostWidth = (containerWidth/columns)+200;
-    // var smallPostWidth = (containerWidth/columns)-200;
 		postWidth = Math.floor(postWidth);
-    // largePostWidth = Math.floor(largePostWidth);
-    // smallPostWidth = Math.floor(smallPostWidth);
 
     $(".grid-item").each(function(index){
 			$(this).css({"width":postWidth+"px"});
 		});
-
-    // $(".grid-item.grid-item--width1").each(function(index){
-    //   $(this).css({"width":smallPostWidth+"px"});
-    // });
-    //
-    // $(".grid-item.grid-item--width2").each(function(index){
-		// 	$(this).css({"width":largePostWidth+"px"});
-		// });
 
 	}
 
